344ab35ee2abc687461d7c03aadf5c175feef887
mkcnelis <maikel.nelis@gmail.com>
2023-06-13 10:09:56 +0200

    begin for converting site

---
+++ JavaSript_files/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', 
+    function(){
+      Typed.new('.entry', {
+        strings: [ "Portfolio Maikel",],
+        typeSpeed: 1
+      });
+  });
+  
+
+
---
+++ JavaSript_files/script1.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', 
+    function(){
+      Typed.new('.entry', {
+        strings: [ "Portfolio"+"<br>"+" maikel",],
+        typeSpeed: 1
+      });
+  });
+  
+
+
+  document.addEventListener('DOMContentLoaded', //the second  ling of entries of scroll text
+  function(){
+    Typed.new('.entry2', {
+      strings: [ " ​Mijn naam is Maikel Kriangsak Chandeduangdee Nelis, ik ben geboren op 21 November 1997 in Bangkok, Thailand",],
+      typeSpeed: 1
+    });
+});
